366d585ebec1aa4dcf33a73f54e3349ee8653a6f
RolandRozsa <roland.rozsa@icellmobilsoft.hu>
2023-07-11 15:55:05 +0200

    English translation of documentation and documents #62
    Common

--- common/common-system-rest/src/main/java/hu/icellmobilsoft/sampler/common/system/rest/validation/catalog/Starter.java
+++ common/common-system-rest/src/main/java/hu/icellmobilsoft/sampler/common/system/rest/validation/catalog/Starter.java
@@ -59,11 +59,11 @@ public class Starter {
             // Try doing it the normal way
             URL.setURLStreamHandlerFactory(delegator);
         } catch (final Error e) {
-            // Force it via reflection (eleg durva hacking, de minden mas megoldas nem mukodik)
+            // Force it via reflection (it is quite rough hacking, but no other solution is working)
             try {
                 final Field factoryField = URL.class.getDeclaredField("factory");
                 factoryField.setAccessible(true);
-                // A már beállított (default) factory-t beállítjuk a delegatornak, hogy az is tudjon futni
+                // We set the already configured (default) factory to the delegator so that it can also run
                 URLStreamHandlerFactory factory = (URLStreamHandlerFactory) factoryField.get(null);
                 delegator.addUrlStreamHandlerFactory(factory);
                 factoryField.set(null, delegator);
